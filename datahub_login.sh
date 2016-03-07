a1c23ef37bbba0c7e86edb811473eda6302d5309
yepengxj <yepeng_xj@163.com>
2016-03-07 21:54:02 +0800

    Create datahub_login.sh

---
+++ datahub_login.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+datahub --daemon
+user=$1
+echo "user:"$user
+pass=$2
+echo "pass:"$pass
+VAR=$(expect -c "
+spawn datahub login
+expect \"*?login:*\"
+send -- \"$user\r\"
+expect \"*?password:*\"
+send -- \"$pass\r\"
+send -- \"\r\"
+expect eof
+")
+
+echo $VAR
+echo "============"
